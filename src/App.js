77dcd413ba83cc3c816f53d33f9ee59bb8e25a59
rootgh <95168267+Aliko2020@users.noreply.github.com>
2023-12-14 17:14:24 -0800

    added comments

--- src/App.js
+++ src/App.js
@@ -2,36 +2,61 @@ import './App.css';
 import { Header } from './Header';
 import { Todos } from './Todos';
 import { ButtomNav } from './ButtomNav';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form } from './Form';
 
 function App() {
-const [darkMode, setDarkMode] = useState(true)
+  // State for controlling dark mode
+  const [darkMode, setDarkMode] = useState(true);
 
-function toggleDarkMode(){
-    setDarkMode(!darkMode)
-} 
+  // State for managing the list of todos
+  const [todos, setTodos] = useState(() => {
+    // Retrieve todos from localStorage or use a default value if not present
+    const storedTodos = localStorage.getItem('todos');
+    return storedTodos ? JSON.parse(storedTodos) : ['Jog around the park 3x', '10 minutes meditation', 'Complete Todo app'];
+  });
 
-const [todos, setTodos] = useState([
-  'Jog around the park 3x',
-  '10 minutes meditation',
-  'Complete Todo app'
-])
+  // Effect hook to save todos to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-function addTodo(formInput, setFormInput){
-  if (formInput.trim() !== '') {
-    setTodos([formInput,...todos]);
-    setFormInput('');
+  // Function to toggle dark mode
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
   }
-};
+
+  // Function to update todos
+  function updateTodos(newTodos) {
+    setTodos(newTodos);
+  }
+
+  // Function to add a new todo
+  function addTodo(formInput, setFormInput) {
+    if (formInput.trim() !== '') {
+      setTodos([formInput, ...todos]);
+      setFormInput('');
+    }
+  }
+
   return (
     <div className="App">
+      {/* Header Component */}
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <div className='Main'>
+
+      <div className="Main">
+        {/* Form Component for adding new todos */}
         <Form addTodo={addTodo} darkMode={darkMode} />
-        <Todos todos={todos} darkMode={darkMode} />
+
+        {/* Todos Component for displaying and managing todos */}
+        <Todos todos={todos} darkMode={darkMode} setTodos={updateTodos} />
+
+        {/* Bottom Navigation Component */}
         <ButtomNav darkMode={darkMode} />
       </div>
+
+      {/* Attribution */}
+      <p className='buttom-p'>Challenge by <a href="https://www.frontendmentor.io/">Frontend Mentor</a> Coded by <a href="https://www.linkedin.com/in/aliko-amos-0a7098246/">Aliko Amos</a></p>
     </div>
   );
 }
--- src/Todos.js
+++ src/Todos.js
@@ -1,43 +1,62 @@
 import React, { useState } from 'react';
 import cross from '../src/images/icon-cross.svg';
-import crossLight from './images/icon-crosslight.svg'
+import crossLight from './images/icon-crosslight.svg';
 import checkSvg from './images/icon-check.svg';
 
-export const Todos = ({ todos, darkMode }) => {
+export const Todos = ({ todos, darkMode, setTodos }) => {
+  // State to manage checked todos
   const [checkedTodos, setCheckedTodos] = useState([]);
 
+  // Function to toggle the completion status of a todo
   const toggleTodo = (index) => {
     if (checkedTodos.includes(index)) {
+      // If the todo is already checked, remove it from the list
       setCheckedTodos(checkedTodos.filter((i) => i !== index));
     } else {
+      // If the todo is not checked, add it to the list
       setCheckedTodos([...checkedTodos, index]);
     }
   };
 
+  // Function to remove a todo
+  const removeTodo = (index) => {
+    // Remove the todo from the todos list
+    setTodos(todos.filter((_, i) => i !== index));
+    // Optionally, you can also update the checkedTodos state if needed
+    setCheckedTodos(checkedTodos.filter((i) => i !== index));
+  };
+
   return (
-    <div className={darkMode ? "todos-container-dark" : "todos-container-light"}>
+    <div className={darkMode ? 'todos-container-dark' : 'todos-container-light'}>
       <ul>
+        {/* Map through todos and create list items */}
         {todos.map((todo, index) => (
           <li
             key={index}
-            className={`${darkMode ? "todo-item-dark" : "todo-item-light"} ${checkedTodos.includes(index) ? 'checked' : ''}`}
+            className={`${darkMode ? 'todo-item-dark' : 'todo-item-light'} ${
+              checkedTodos.includes(index) ? 'checked' : ''
+            }`}
             onClick={() => toggleTodo(index)}
           >
-            <div className='todo'>
-              <div className={darkMode ? "rounded-box-dark" : "rounded-box-light"}>
-                <img src={checkSvg} alt='check svg' />
+            <div className="todo">
+              {/* Display checkbox */}
+              <div className={darkMode ? 'rounded-box-dark' : 'rounded-box-light'}>
+                <img src={checkSvg} alt="check svg" />
               </div>
+              {/* Display todo text */}
               {todo}
             </div>
-            <div>
-              {darkMode ? <img src={cross} alt='cross svg' /> : <img src={crossLight} alt='cross svg' />}
+            {/* Display remove icon */}
+            <div onClick={() => removeTodo(index)}>
+              {darkMode ? <img src={cross} alt="cross svg" /> : <img src={crossLight} alt="cross svg" />}
             </div>
           </li>
         ))}
-        <li className='todo-item-dark'>
-          <div className={darkMode ? "todo-notification-dark" : "todo-notification-light"}>
-            <p className='notification'>{todos.length} Task{todos.length > 1 ? "s" :""} left</p>
-            <button >Clear Completed</button>
+        {/* Display the task count and Clear Completed button */}
+        <li className="todo-item-dark">
+          <div className={darkMode ? 'todo-notification-dark' : 'todo-notification-light'}>
+            <p className="notification">{todos.length} Task{todos.length > 1 ? 's' : ''} left</p>
+            <button>Clear Completed</button>
           </div>
         </li>
       </ul>
